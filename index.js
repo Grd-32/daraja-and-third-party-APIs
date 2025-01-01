3434559ac3ce569e7e6b017778d0b5354ddb6f26
Grd-32 <pfdaniel77@gmail.com>
2025-01-01 22:54:31 +0300

    addind cron jobs to import tenders daily

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ import './utils/scheduler.js';
 import { loadInitialData } from './controllers/tenderController.js';
 import paymentRoutes from './routes/paymentRoutes.js';
 
-// Load environment variables
-dotenv.config();
+dotenv.config(); // Load environment variables
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use(errorMiddleware);
 
@@ -27,22 +30,23 @@ app.use('/api/tenders', tenderRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/payment', paymentRoutes);
 
-// Database Connection
+// Database Connection and Server Start
 const startServer = async () => {
-    try {
-      await mongoose.connect('mongodb+srv://danfdev6:Danf102020!@cluster0.yeci2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
-      console.log('Connected to MongoDB');
-  
-      // Load initial data
-      await loadInitialData();
-  
-      // Start server
-      app.listen(process.env.PORT || 5000, () => {
-        console.log(`Server running on port ${process.env.PORT || 5000}`);
-      });
-    } catch (error) {
-      console.error('Failed to start the server:', error);
-    }
-  };
-  
-  startServer();
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDB');
+
+    // Load initial data
+    await loadInitialData();
+
+    // Start server
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Failed to start the server:', error);
+  }
+};
+
+startServer();
